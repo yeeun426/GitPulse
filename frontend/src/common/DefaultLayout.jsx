8a884f472d6507645d6dc67bd803890e75ca7a11
yeeun426 <thsudkcla7@naver.com>
2025-05-09 13:19:59 +0900

    feat : default layout sidebar UI

--- frontend/src/common/DefaultLayout.jsx
+++ frontend/src/common/DefaultLayout.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-
+import SideTab from "./SideBar";
 import "./index.css";
 import css from "./DefaultLayout.module.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 export const DefaultLayout = () => {
   return (
     <div className={css.defaultLayout}>
+      <SideTab />
       <Outlet />
-      <footer>copyrights</footer>
     </div>
   );
 };
