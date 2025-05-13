e8525b409f3b4347b939d5aedd534a96419fca93
yeeun426 <thsudkcla7@naver.com>
2025-05-13 11:02:24 +0900

    feat : repo select

--- frontend/src/pages/OrganizationPage.jsx
+++ frontend/src/pages/OrganizationPage.jsx
@@ -1,16 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import css from "./ProfilePage.module.css";
+import orgs from "./OrganizationPage.module.css";
 import Header from "../components/Header";
 import { useOrgsInfo } from "../apis/useOrganizationApi";
 
 const OrganizationPage = () => {
   const { name } = useParams();
-
   const { data, isLoading, isError } = useOrgsInfo(name);
 
   const members = data?.members;
   const repos = data?.repos;
+
+  const [selected, setSelected] = useState("");
+
+  useEffect(() => {
+    if (repos && repos.length > 0) {
+      setSelected(repos[0].name);
+    }
+  }, [repos]);
+
+  isLoading && <p>Loading</p>;
+  isError && <p>에러 발생</p>;
 
   return (
     <div className={css.container}>
@@ -18,16 +29,26 @@ const OrganizationPage = () => {
         {/* 헤더영역  + desc 추가 예정 */}
         <Header name={name} />
 
-        {/* orgs info 영역 */}
         <div className={css.contentContainer}>
+          {/* orgs info 영역 */}
           <section className={css.profileStats}>
             <div className={css.card}>
               <i className="bi bi-cloud-check"></i>
               <div className={css.cardText}>
                 <p className={css.cardLabel}>현재 레포지토리</p>
-                {repos?.length > 0 && (
-                  <p className={css.cardValue}>{repos[0]?.name}</p>
-                )}
+                <select onChange={(e) => setSelected(e.target.value)}>
+                  {repos?.map((item, index) =>
+                    item?.name ? (
+                      <option
+                        className={orgs.cardValue}
+                        key={index}
+                        value={item.name}
+                      >
+                        {item.name}
+                      </option>
+                    ) : null
+                  )}
+                </select>
               </div>
             </div>
             <div className={css.card}>
