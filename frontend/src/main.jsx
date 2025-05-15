a8e715d0418eb40e8ec4f09c7b9d756960d7fe09
alex8396 <alex8396@naver.com>
2025-05-15 13:29:48 +0900

    feat:chaellge 페이지 추가

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,7 +4,7 @@ import { RouterProvider } from "react-router-dom";
 import { router } from "./router/index";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 // 전역에서 Query를 관리할 QueryClient 생성
 const queryClient = new QueryClient();
 
