95d7e8d862dc6cdfe54dbb2bc40cdfe5b797f5d6
박용규 <yoyo5411@naver.com>
2025-05-16 11:52:51 +0900

    Feat : DevTestPage로 분리, TestIntro, DevTest, DevResult 반응형 적용

--- frontend/src/components/IntroPage.jsx
+++ frontend/src/components/IntroPage.jsx
@@ -1,26 +1,20 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import css from "./IntroPage.module.css";
 import logoImage from "../assets/icon_mini.png"; // 이미지 파일 경로 확인!
+import SideBar from "../common/SideBar";
 
-const IntroPage = () => {
-  const navigate = useNavigate();
-
-  const handleStart = () => {
-    navigate("/teststart"); // 테스트 페이지 경로로 이동
-  };
-
+const IntroPage = ({ onStart }) => {
   return (
-    <div className={css.container}>
+    <main className={css.container}>
       <h1 className={css.title}>개발자 유형 테스트</h1>
       <div className={css.logoWrapper}>
         <img src={logoImage} alt="GitPulse Logo" className={css.logo} />
       </div>
       <p className={css.subtitle}>나의 개발자 유형과 숨겨진 능력은 ?</p>
-      <button className={css.startButton} onClick={handleStart}>
+      <button onClick={onStart} className={css.startButton}>
         테스트 시작하기
       </button>
-    </div>
+    </main>
   );
 };
 
